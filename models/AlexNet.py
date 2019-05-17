b9ae1a3af0815e04de0fe3bd802d16240b2c4c7e
Robert <hhroberthdaniel>
2019-05-17 17:30:46 +0300

    pumped up alex net

--- models/AlexNet.py
+++ models/AlexNet.py
@@ -1,38 +1,52 @@
 import torch.nn as nn
-
+from torchvision import transforms
 '''
 modified to fit dataset size
 '''
 NUM_CLASSES = 10
 
+class DivByMax(nn.Module):
+    def __init__(self, dim, pow=1):
+        super().__init__()
+        self.pow = pow
+        self.dim = dim
+
+    def forward(self, x):
+        x = x ** self.po
+        sums = x.sum(dim=self.dim)
+        x = x / (sums + 1e-20)
+        return x
 
 class AlexNet(nn.Module):
     def __init__(self, num_classes=NUM_CLASSES):
         super(AlexNet, self).__init__()
         self.initial_bn = nn.BatchNorm2d(3)
         self.features = nn.Sequential(
+
             nn.Conv2d(3, 64, kernel_size=3, stride=2, padding=1),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2),
-            nn.Conv2d(64, 192, kernel_size=3, padding=1),
+            nn.Conv2d(64, 512, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2),
-            nn.Conv2d(192, 384, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.Conv2d(512, 1024, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
-            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            nn.Conv2d(1024, 256, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
             nn.Conv2d(256, 256, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2),
         )
         self.classifier = nn.Sequential(
-            nn.Dropout(),
-            nn.Linear(256 * 2 * 2, 4096),
+            # nn.Dropout(),
+            nn.Linear(256 * 2 * 2, 256),
             nn.ReLU(inplace=True),
-            nn.Dropout(),
-            nn.Linear(4096, 4096),
+            # nn.Dropout(),
+            nn.Linear(256, 256),
             nn.ReLU(inplace=True),
-            nn.Linear(4096, num_classes),
+            nn.Linear(256, num_classes),
+            # nn.Softmax(),
         )
 
     def forward(self, x):
